7715bff4a77f9c8f2431df92826f510c630ce564
Xiangyang Ju <xiangyang.ju@gmail.com>
2022-07-15 17:07:07 -0700

    more data

--- acctrack/io/__init__.py
+++ acctrack/io/__init__.py
@@ -1,5 +1,10 @@
 from collections import namedtuple
 
+__all__ = ['TrackMLReader', 'ActsReader', "MeasurementData"]
+
+from acctrack.io.trackml import TrackMLReader
+from acctrack.io.acts import ActsReader
+
 MeasurementData = namedtuple('MeasurementData', [
     'hits', 'measurements', 'meas2hits', 'spacepoints',
     'particles', 'true_edges', 'event_file'])
--- acctrack/scripts/process_data.py
+++ acctrack/scripts/process_data.py
@@ -5,7 +5,7 @@ from multiprocessing import Pool
 from functools import partial
 
 from acctrack.io.utils import save_to_np
-from acctrack.io.acts import ActsReader
+from acctrack import io
 
 
 def process(evtid, reader, output_dir):
@@ -22,9 +22,16 @@ if __name__ == "__main__":
     add_arg("out_dir", help="output directory")
     add_arg("-w", "--workers", help='number of workers', default=1, type=int)
     add_arg("--spname", help='spacepoint name', default='spacepoints')
+    add_arg("--reader", help='reader class', default=None, choices=list(io.__all__))
     args = parser.parse_args()
 
-    reader = ActsReader(args.in_dir, args.spname)
+    if args.reader is None:
+        print("Specify a data type via --reader")
+        parser.print_help()
+        exit(1)
+
+
+    reader = getattr(io, args.reader)(args.in_dir, args.spname)
 
     if args.workers < 2:
         for evtid in reader.all_evtids:

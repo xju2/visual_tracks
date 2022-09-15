987094543534fe977dc5fd0252ceab986412405d
Xiangyang Ju <xiangyang.ju@gmail.com>
2022-09-15 13:38:54 -0700

    up

--- acctrack/viewer/view.py
+++ acctrack/viewer/view.py
@@ -8,6 +8,7 @@ def plot(track: np.ndarray,
         figsize=(15,12),
         fontsize=16,
         minorsize=14,
+        unit='mm',
         outname=None, **kwargs) -> None:
     """Plot a track from different views"""
 
@@ -31,27 +32,28 @@ def plot(track: np.ndarray,
 
 
     ## plot r vs z
-    ax1.plot(z, r, **kwargs)
-    ax1.set_xlabel("z [m]", fontsize=fontsize)
-    ax1.set_ylabel("r [m]", fontsize=fontsize)
+    ax1.plot(z, r, '-*', **kwargs)
+    ax1.set_xlabel(f"z [{unit}]", fontsize=fontsize)
+    ax1.set_ylabel(f"r [{unit}]", fontsize=fontsize)
     ax1.tick_params(axis='both', which='major', labelsize=minorsize)
 
     ## plot x vs y
-    ax2.plot(x, y, **kwargs)
-    ax2.set_xlabel("x [m]", fontsize=fontsize)
-    ax2.set_ylabel("y [m]", fontsize=fontsize)
+    ax2.plot(x, y, '-*', **kwargs)
+    ax2.set_xlabel(f"x [{unit}]", fontsize=fontsize)
+    ax2.set_ylabel(f"y [{unit}]", fontsize=fontsize)
     ax2.tick_params(axis='both', which='major', labelsize=minorsize)
 
     ## plot 3D
     ax3.plot3D(x, y, z, **kwargs)
-    ax3.set_xlabel("x [m]", fontsize=fontsize)
-    ax3.set_ylabel("y [m]", fontsize=fontsize)
-    ax3.set_zlabel("z [m]", fontsize=fontsize)
+    ax3.set_xlabel(f"x [{unit}]", fontsize=fontsize)
+    ax3.set_ylabel(f"y [{unit}]", fontsize=fontsize)
+    ax3.set_zlabel(f"z [{unit}]", fontsize=fontsize)
     ax3.tick_params(axis='both', which='major', labelsize=minorsize)
     ax3.scatter(x, y, z, **kwargs)
     
-    ## plot x vs y in polar coordinates
-    ax4.plot(x, y, **kwargs)
-    ax4.set_xlabel("x [m]", fontsize=fontsize)
-    ax4.set_ylabel("y [m]", fontsize=fontsize)
+    ## plot r vs phi in polar coordinates
+    phi = np.arctan2(y, x)
+    ax4.plot(r, phi, **kwargs)
+    ax4.set_xlabel(f"x [{unit}]", fontsize=fontsize)
+    ax4.set_ylabel(f"y [{unit}]", fontsize=fontsize)
     ax4.tick_params(axis='both', which='major', labelsize=minorsize)

6e58386e2d12711f31327fb1eab36c9bbf07ce3a
Xiangyang Ju <xju@lbl.gov>
2024-11-18 21:23:08 -0800

    run all tests

--- scripts/clean_athena_run.sh
+++ scripts/clean_athena_run.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
 rm InDetIdDict.xml PoolFileCatalog.xml eventLoopHeartBeat.txt hostnamelookup.tmp \
-  jobReport.json perfmonmt_RAWtoALL.json.tar.gz prmon.* my_setupATLAS.sh \
+  jobReport.json my_setupATLAS.sh \
   runargs.RAWtoALL.py runwrapper.RAWtoALL.sh log.RAWtoALL
+
+
+# perfmonmt_RAWtoALL.json.tar.gz prmon.*
--- scripts/run_tracking.sh
+++ scripts/run_tracking.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
 
 # RDO_FILENAME="../run_main_clean/reduced_RDO.r14701.33629030._000008.pool.root.1"
-RDO_FILENAME="inputData/RDO.37737772._000213.pool.root.1"
+#RDO_FILENAME="inputData/RDO.37737772._000213.pool.root.1"
+
+## release 24 ttbar events for evaluation
+RDO_FILENAME="/global/cfs/cdirs/m3443/data/GNN4ITK/RDOFiless/rel24_ttbar_testing/RDO.37737772._000213.pool.root.1"
+
+## datasets for generality studies
+MuonPU0=$(\ls /global/cfs/cdirs/m3443/data/GNN4ITK/RDOFiless/mc21_14TeV.900495.PG_single_muonpm_Pt10_etaFlatnp0_43.recon.RDO.e8481_s4149_r14697/* | paste -sd ',')
+ZmumuPU0=$(\ls /global/cfs/cdirs/m3443/data/GNN4ITK/RDOFiless/mc21_14TeV.601190.PhPy8EG_AZNLO_Zmumu.recon.RDO.e8481_s4203_r14697/* | paste -sd ',')
+ZmumuPU200=$(\ls /global/cfs/cdirs/m3443/data/GNN4ITK/RDOFiless/mc21_14TeV.601190.PhPy8EG_AZNLO_Zmumu.recon.RDO.e8481_s4203_r14701/* | paste -sd ',')
+HaaPU0=$(\ls /global/cfs/cdirs/m3443/data/GNN4ITK/RDOFiless/mc21_14TeV.603008.PhPy8_ggH_H125_a55a55_4b_ctau100.recon.RDO.e8481_s4149_r14697/* | paste -sd ',')
+HaaPU200=$(\ls /global/cfs/cdirs/m3443/data/GNN4ITK/RDOFiless/mc21_14TeV.603008.PhPy8_ggH_H125_a55a55_4b_ctau100.recon.RDO.e8481_s4149_r14700/* | paste -sd ',')
+
+
+#echo $RDO_FILENAME
 
 function clean_up() {
     rm InDetIdDict.xml PoolFileCatalog.xml hostnamelookup.tmp eventLoopHeartBeat.txt
@@ -154,34 +167,86 @@ function metric_learning_tracking() {
     clean_up
     export ATHENA_CORE_NUMBER=1
     ModelName="MetricLearning"
-    TritionServer="nid200509"
-    # --skipEvents 44
-    # 'InDetConfig.ConfigurationHelpers.OnlyTrackingPreInclude'
-    # --preExec 'flags.Tracking.GNN.usePixelHitsOnly = True; from AthOnnxComps.OnnxRuntimeFlags import OnnxRuntimeType; flags.AthOnnx.ExecutionProvider = OnnxRuntimeType.CUDA' \
-    # --postExec 'all:cfg.getService("AlgResourcePool").CountAlgorithmInstanceMisses = True' \
-    # --perfmon 'fullmonmt' \
+    TritionServer="nid008229"
+
+    if [ "$#" -ne 4 ]; then
+        echo "Usage: $0 <input_file> <out_file> <log_file> <process_name>"
+        return 1
+    fi
+
+	INPUT_FILE=$1
+    OUT_FILE=$2
+    LOG_FILE=$3
+    TAG_NAME=$4
+
 
     Reco_tf.py \
         --CA 'all:True' --autoConfiguration 'everything' \
         --conditionsTag 'all:OFLCOND-MC15c-SDR-14-05' \
         --geometryVersion 'all:ATLAS-P2-RUN4-03-00-00' \
-        --multithreaded 'False' \
+        --multithreaded 'True' \
         --steering 'doRAWtoALL' \
         --digiSteeringConf 'StandardInTimeOnlyTruth' \
         --postInclude 'all:PyJobTransforms.UseFrontier' \
         --preInclude 'all:Campaigns.PhaseIIPileUp200' 'InDetConfig.ConfigurationHelpers.OnlyTrackingPreInclude' 'InDetGNNTracking.InDetGNNTrackingFlags.gnnTritonValidation' \
         --preExec "flags.Tracking.GNN.Triton.model = \"${ModelName}\"; flags.Tracking.GNN.Triton.url = \"${TritionServer}\"; flags.Tracking.ITkGNNPass.doAmbiguityResolutionForGNN = False;" 'flags.Tracking.GNN.TrackFinderTritonTool.FeatureNames = "r,phi,z,cluster_x_1,cluster_y_1,cluster_z_1,cluster_x_2,cluster_y_2,cluster_z_2,count_1,charge_count_1,loc_eta_1,loc_phi_1,localDir0_1,localDir1_1,localDir2_1,lengthDir0_1,lengthDir1_1,lengthDir2_1,glob_eta_1,glob_phi_1,eta_angle_1,phi_angle_1,count_2,charge_count_2,loc_eta_2,loc_phi_2,localDir0_2,localDir1_2,localDir2_2,lengthDir0_2,lengthDir1_2,lengthDir2_2,glob_eta_2,glob_phi_2,eta_angle_2,phi_angle_2,eta,cluster_r_1,cluster_phi_1,cluster_eta_1,cluster_r_2,cluster_phi_2,cluster_eta_2"' \
-        --inputRDOFile "${RDO_FILENAME}" \
-        --outputAODFile 'test.aod.ML.gnnTriton.root'  \
+        --inputRDOFile "${INPUT_FILE}" \
+        --outputAODFile "${OUT_FILE}"  \
         --jobNumber '1' \
 		--athenaopts='--loglevel=INFO' \
-        --maxEvents -1 2>&1 | tee log.ML.gnnTrition.txt
+        --maxEvents -1 2>&1 | tee ${LOG_FILE}
+
+    ResultDIR="Results_20241118"
+    ./clean_athena_run.sh
+    mkdir -p $ResultDIR/${TAG_NAME}
+    mv $OUT_FILE $LOG_FILE prmon.* perfmonmt* $ResultDIR/${TAG_NAME}/
+}
+
+function gnn_mm_tracking() {
+    clean_up
+    export ATHENA_CORE_NUMBER=10
+
+    Reco_tf.py \
+        --CA 'all:True' --autoConfiguration 'everything' \
+        --conditionsTag 'all:OFLCOND-MC15c-SDR-14-05' \
+        --geometryVersion 'all:ATLAS-P2-RUN4-03-00-00' \
+        --multithreaded 'True' \
+        --steering 'doRAWtoALL' \
+        --digiSteeringConf 'StandardInTimeOnlyTruth' \
+        --postInclude 'all:PyJobTransforms.UseFrontier' \
+        --preInclude 'all:Campaigns.PhaseIIPileUp200' 'InDetConfig.ConfigurationHelpers.OnlyTrackingPreInclude' 'InDetGNNTracking.InDetGNNTrackingFlags.gnnReaderValidation' \
+        --preExec "flags.Tracking.ITkGNNPass.doAmbiguityResolutionForGNN = False; flags.Tracking.GNN.TrackReader.inputTracksDir = \"MM_tracks_with_AMP\"; "  \
+        --inputRDOFile "${RDO_FILENAME}" \
+        --outputAODFile 'test.aod.MM.gnn.root'  \
+        --jobNumber '1' \
+		--athenaopts='--loglevel=INFO' \
+        --maxEvents 1 2>&1 | tee log.MM.gnn.txt
+}
+
+function rename_acorn_outs() {
+    cd MM_tracks_with_AMP
+    for file in event*.txt
+    do
+       # tune to track_242020_eventID.csv
+       eventID=$(echo $file | cut -d'/' -f 2 | cut -d"." -f 1 | sed 's/event00//')
+       ln -s "$file" "track_242020_${eventID}.csv"
+    done
 }
 
 # time gnn4pixel
 # time gnn4pixel_with_dumping
 # time gnn4pixel_with_AR
-time metric_learning_tracking
+
+# time metric_learning_tracking $RDO_FILENAME test.aod.ML.gnnTriton.root log.ML.gnnTrition.txt Testing
+
+time metric_learning_tracking "$ZmumuPU0" aod.ML.ZmumuPU0.root log.ML.ZmumuPU0.txt ZmumuPU0
+time metric_learning_tracking "$ZmumuPU200" aod.ML.ZmumuPU200.root log.ML.ZmumuPU200.txt ZmumuPU200
+time metric_learning_tracking "$HaaPU0" aod.ML.HaaPU0.root log.ML.HaaPU0.txt HaaPU0
+time metric_learning_tracking "$HaaPU200" aod.ML.HaaPU200.root log.ML.HaaPU200.txt HaaPU200
+
+# rename_acorn_outs
+
+# gnn_mm_tracking
 
 # time ckf_tracking
 
@@ -189,3 +254,4 @@ time metric_learning_tracking
 # run_idpvm AllEvents/ckf/test.aod.ckf.debug.root AllEvents/ckf/test.idpvm.root
 # run_idpvm AllEvents/gnn/test.aod.gnnreader.root AllEvents/gnn/physval.root
 # run_idpvm AllEvents/gnn/aod.noAR.50evts.root AllEvents/gnn/physval.noAR.50evts.root
+# run_idpvm AllEvents/end2end_ML/test.aod.ML.gnnTriton.root AllEvents/end2end_ML/physval.root

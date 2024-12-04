9dbfec2ba7b23f756eaa33c3001504b4e0d8eebc
Xiangyang Ju <xju@lbl.gov>
2024-12-04 12:21:56 -0800

    update versin

--- scripts/install_pyg.sh
+++ scripts/install_pyg.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-pip install torch==2.5.1
-pip install pyg_lib torch_scatter torch_sparse torch_cluster torch_spline_conv -f https://data.pyg.org/whl/torch-2.5.1+cu124.html
-pip install torch_geometric
+pip install torch==2.5.1 --index-url https://download.pytorch.org/whl/cu121
+pip install  --no-cache-dir --force-reinstall pyg_lib torch_scatter torch_sparse torch_cluster torch_spline_conv -f https://data.pyg.org/whl/torch-2.5.1+cu121.html
+pip install torch_geometric==2.6.1
